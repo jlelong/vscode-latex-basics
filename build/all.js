014ebf8cae6fea024b2d9ee88d99213736d8cd53
Jerome Lelong <jerome.lelong@gmail.com>
2022-07-17 15:12:41 +0200

    Add documentation and call script within build stage

--- build/all.js
+++ build/all.js
@@ -1,5 +1,8 @@
 const bailout = require('./generate-bailout')
 const md = require('./latex-md')
+const codeblocks = require('./generate-grammar-blocks')
 
+/* Make sure to generate grammar blocks first */
+codeblocks()
 bailout()
 md()
